b494f14b035168540ef5abaca74fc722349e7d23
PingAnNetBar <840495823@qq.com>
2018-08-07 21:37:33 +0800

    aa

---
+++ Js/Js.js
@@ -0,0 +1,90 @@
+window.onload = function(){
+    
+    /*封装函数 */
+    function ById(id){
+        return typeof(id)? document.getElementById(id):'id';
+    }
+    function ByTagName(tagName){
+        return typeof(tagName)? document.getElementsByTagName(tagName):'tagName'
+    }
+
+    var container = ById('container'),
+        list = ById('list'),
+        prev = ById('prev'),
+        next = ById('next'),
+        timer,
+        index = 1;
+    var buttons = ByTagName('span');
+
+        
+    
+    /* 偏移值变换*/
+    function mov(value){
+        var newLeft = parseInt(list.style.left) + value;
+        list.style.left = newLeft + 'px';
+    /* 最左最右判断*/
+        if(newLeft<-4000){
+            list.style.left = 0 + 'px';
+        }
+        if(newLeft>0){
+            list.style.left = -4000 + 'px';
+        }
+    }
+    /* 左右箭头功能*/
+    prev.onclick = function(){
+        index-=1;
+        if(index<1){
+            index = 5;
+        }
+        mov(1000);
+        buttonsShow();
+    }
+    next.onclick = function(){
+        index+=1;
+        if(index>5){
+            index = 1;
+        }
+        mov(-1000);
+        buttonsShow();
+    }
+    /* 自动轮播功能*/
+    function play(){
+        timer = setInterval(function (){
+            next.onclick();      
+        },1500);
+    }
+    play();
+
+    /* q轮播功能*/
+    function stop(){
+        clearInterval(timer);
+    }
+
+    container.onmouseover = stop;
+    container.onmouseout = play;
+
+    /* 圆点响应 */
+   
+    function buttonsShow(){
+        for( var i=0; i<buttons.length;i++){
+            if(buttons[i].className = 'on'){
+                buttons[i].className = '';
+            }
+        }
+        buttons[index-1].className = 'on';
+    }
+    for (var i = 0; i < buttons.length; i++) {
+        // 这里使用的是立即执行函数，
+        (function(i) {
+            buttons[i].onclick = function() {
+                var clickIndex = parseInt(this.getAttribute('index'));
+                console.log(clickIndex);
+                
+                var value = 1000 * (index - clickIndex); 
+                mov(value);
+                index = clickIndex;
+                buttonsShow();
+            }
+        })(i)
+    }
+}
